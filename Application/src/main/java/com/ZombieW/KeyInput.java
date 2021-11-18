90e4c8dd3f0f171382a04e6c973ea9fb45e4d112
gurdaanm <gurdaanm@sfu.ca>
2021-11-18 03:49:48 +0000

    Update KeyInput.java

--- Application/src/main/java/com/ZombieW/KeyInput.java
+++ Application/src/main/java/com/ZombieW/KeyInput.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 /**
  *Class which contains how the player will move up, down, left, and right.
  *contains two sub classes which allow this to work.
-*/
+ */
 public class KeyInput implements KeyListener {
 
     public boolean up, down, left, right;
@@ -14,10 +14,11 @@ public class KeyInput implements KeyListener {
     public void keyTyped(KeyEvent e) {
     }
     /**
-     *This class is used for when a key is pressed and allows the character to move.
-     *You move up using W, down used S, left using A, and right using D.
-     *Class also changes the boolean value assigned to being true, so the player can move.
-    */
+     * This class is used for when a key is pressed and allows the character to move.
+     * You move up using W, down used S, left using A, and right using D.
+     * Class also changes the boolean value assigned to being true, so the player can move.
+     * @param e The key which is pressed to tell player which direction to move.
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
@@ -40,10 +41,11 @@ public class KeyInput implements KeyListener {
         }
     }
     /**
-     *This class is used for when the key that is pressed is released. 
-     *Once the key is released the player will stop going in that direction.
-     *Also changes the boolean value of the assigned keys to false for them to stop.
-    */
+     * This class is used for when the key that is pressed is released 
+     * Once the key is released the player will stop going in that direction
+     * Also changes the boolean value of the assigned keys to false for them to stop
+     * @param e The key pressed which tells the player which way to move
+     */
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
