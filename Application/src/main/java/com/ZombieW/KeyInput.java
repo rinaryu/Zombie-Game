62483ce88e6007c9d73ab7a09aa49b28f88e61b7
gurdaanm <gurdaanm@sfu.ca>
2021-11-18 02:32:10 +0000

    added comments

--- Application/src/main/java/com/ZombieW/KeyInput.java
+++ Application/src/main/java/com/ZombieW/KeyInput.java
@@ -2,7 +2,8 @@ package com.ZombieW;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+//Class which contains how the player will move up, down, left, and right.
+//contains two sub classes which allow this to work.
 public class KeyInput implements KeyListener {
 
     public boolean up, down, left, right;
@@ -10,7 +11,9 @@ public class KeyInput implements KeyListener {
     @Override
     public void keyTyped(KeyEvent e) {
     }
-
+    //This class is used for when a key is pressed and allows the character to move.
+    //You move up using W, down used S, left using A, and right using D.
+    //Class also changes the boolean value assigned to being true, so the player can move.
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
@@ -32,7 +35,9 @@ public class KeyInput implements KeyListener {
             System.out.println("pressed right");
         }
     }
-
+    //This class is used for when the key that is pressed is released. 
+    //Once the key is released the player will stop going in that direction.
+    //Also changes the boolean value of the assigned keys to false for them to stop.
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
