bd607a28a4c072d751eaa11f6773ce81ba065ffa
Rina <rra51@sfu.ca>
2021-11-30 11:06:49 -0800

    Starting JUnit testing

---
+++ Application/src/test/java/GameTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GameTest {
+
+    @Test
+    public void demoTest() { // making sure JUnit works
+        assertTrue(true);
+    }
+
+    @Test
+    public void demoTestTwo() { // intentionally false test case
+        assertTrue(false);
+    }
+
+    
+}
