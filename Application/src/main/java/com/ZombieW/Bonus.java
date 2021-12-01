856d0c52d5950fde6d9d506e1350a3599de16e69
bcl19 <brian_lin@sfu.ca>
2021-11-30 17:07:39 -0800

    Added Bonus Reward

--- Application/src/main/java/com/ZombieW/Bonus.java
+++ Application/src/main/java/com/ZombieW/Bonus.java
@@ -1,12 +1,14 @@
 package com.ZombieW;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.IOException;
 
 public class Bonus extends Reward{
-    private int pointsRewarded = 20;
+
     public Bonus(GamePanel gamePanel) {
         super(gamePanel);
+        pointsRewarded = 20;
         getRewardImg();
     }
     private void getRewardImg() {
@@ -20,6 +22,12 @@ public class Bonus extends Reward{
         if(this.x == gamePanel.mc.x && this.y == gamePanel.mc.y && !collected){
             gamePanel.mc.score += getPoints();
             setCollectionStatus(true);
+        }
+    }
+    public void draw(Graphics2D g2){
+//        BufferedImage image = reward;
+        if(!getCollectStatus()) {
+            g2.drawImage(reward, x, y, gamePanel.tileSize-5, gamePanel.tileSize-10, null);
         }
     }
 }
